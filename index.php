86094dd59f6be4654d74ce2eff6d3bcdabb56082
steven-grip <steven@grip-principle.com>
2021-03-24 20:40:36 +0700

    add css styling

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Manfaat Menabung untuk Milenial dan Gen Z</title>
+</head>
+<body>
+    <div class="container">
+        <div class="d-flex flex-row justify-content-center py-3">
+            <h1>Manfaat Menabung untuk Milenial dan Gen Z</h1>
+        </div>
+        <div class="d-flex flex-row justify-content-center py-3 blog-img">
+
+            <img src="https://ibiene.com/wp-content/uploads/2021/03/Bondora.png" alt="">
+        </div>
+        <div class="d-flex flex-row py-3">
+            <p class="text-justify">
+            Menabung adalah menyimpan uang di celengan, bank, dan sebagainya. Namun untuk generasi milenial dan Z yang kerap konsumtif, menabung merupakan hal yang sulit untuk dilakukan. Padahal dari kecil mereka sudah diajarkan untuk menabung. Namun masih saja generasi milenial dan gen Z kesulitan menabung. Milenial adalah mereka yang lahir di rentang waktu 1980-1995. Generasi ini tumbuh di era digital dan dikenal nyaman dengan teknologi dan media sosial. Sedangkan gen Z mereka yang lahir dari rentang waktu 1996-2015. Gen Z tumbuh cenderung tertarik dengan teknologi sejak masih kanak-kanak. Berikut manfaat menabung untuk generasi Milenial dan Gen Z dilansir beberapa sumber:
+            1. Memiliki Simpanan untuk Keadaan Darurat
+
+            Berbagai keadaan darurat atau mendesak sangat mungkin terjadi dalam kehidupan, misalnya kendaraan rusak karena kecelakaan, biaya perawatan rumah sakit, dan masih banyak lainnya. Dengan memiliki tabungan seseorang dapat menggunakan simpanan untuk pengeluaran tidak terduga tanpa harus memotong biaya rutin bulanan.
+            2. Meminimalisir Risiko saat Kehilangan Pekerjaan
+
+            Kehilangan pekerjaan sangat mungkin dialami oleh semua orang. Kebiasaan menabung ini dapat mengurangi perasaan was-was serta risiko yang muncul saat seseorang kehilangan pekerjaan. Setidaknya, uang di tabungan dapat membantu biaya pengeluaran sambil mencari kerja baru atau untuk modal usaha.
+            3. Jaminan Hari Pensiun
+
+            Manfaat menabung selanjutnya adalah sebagai jaminan ketika sudah pensiun. Tentu hal ini berdampak panjang karena seseorang tidak mungkin bekerja selamanya. Dengan menabung sedikit demi sedikit, akan terkumpul simpanan yang cukup untuk hari tua nanti.
+
+            4. Investasi Jangka Panjang
+            Menabung adalah kegiatan jangka panjang karena hasilnya akan terasa dalam jangka panjang. Sebagai contoh seseorang bisa menempatkan tabungan khusus investasi, seperti deposito, reksadana, ataupun bisnis sektor riil yang prospektif lainnya.
+            5. Dana untuk Keperluan yang Diinginkan
+
+            Tabungan tidak hanya untuk keperluan yang dibutuhkan, tetapi bisa juga untuk barang yang diinginkan. Misalnya untuk membeli kendaraan, dana berlibur, biaya kuliah, dan banyak lainnya.
+            6. Memberikan Rasa Tenang
+
+            Manfaat menabung yang tidak kalah pentingnya yakni untuk kebebasan finansial seseorang.Tentu akan ada perasaan tenang, aman, dan nyaman saat memiliki simpanan yang cukup. Hidup juga akan lebih mandiri dan tidak bergantung dengan siapa pun.
+            Sumber: www.detik.com
+            </p>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
