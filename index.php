faab42695a3bb97ee1e8a821dab406879d513742
steven-grip <steven@grip-principle.com>
2021-03-24 20:56:29 +0700

    fix tag

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css" />
     <title>Manfaat Menabung untuk Milenial dan Gen Z</title>
 </head>
 <body>
@@ -15,7 +15,7 @@
         </div>
         <div class="d-flex flex-row justify-content-center py-3 blog-img">
 
-            <img class="img-fluid" src="https://ibiene.com/wp-content/uploads/2021/03/Bondora.png" alt="">
+            <img class="img-fluid" src="https://ibiene.com/wp-content/uploads/2021/03/Bondora.png" alt="" />
         </div>
         <div class="d-flex flex-row py-3">
             <p class="text-justify">
